8177d7dcc1936ff618cafbf556696f81e4e4726b
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2024-09-06 14:30:03 +0100

    Initial Commit

---
+++ Cryptocurrency Predictor/Cryptocurrency_PredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cryptocurrency_PredictorApp.swift
+//  Cryptocurrency Predictor
+//
+//  Created by Veturi, Shreyas (HJRM) on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cryptocurrency_PredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
