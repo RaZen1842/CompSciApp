9d9f4087c24eedee1222660e854d0b716d334be6
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2024-11-19 16:47:48 +0000

    UI Changes

--- Cryptocurrency Predictor/News Screen.swift
+++ Cryptocurrency Predictor/News Screen.swift
@@ -14,31 +14,46 @@ struct News_Screen: View {
     @State private var isThereError: Bool = false
     
     var body: some View {
-        VStack {
-            if isLoading {
-                ProgressView("Loading News")
-            } else if isThereError {
-                Text("Error fetching news")
-            } else {
-                List(newsApi.articles) { article in
-                    VStack(alignment: .leading) {
-                        Text(article.title)
-                            .font(.headline)
-                            .lineLimit(2)
-                        Text(article.description)
-                            .font(.subheadline)
-                            .lineLimit(10)
+        NavigationStack {
+            VStack {
+                Text("News")
+                    .font(.title)
+                    .bold()
+            }
+            VStack {
+                Form {
+                    if isLoading {
+                        Section {
+                            ProgressView("Loading News")
+                        }
+                    } else if isThereError {
+                        Section {
+                            Text("Error fetching news")
+                        }
+                    } else {
+                        Section {
+                            List(newsApi.articles) { article in
+                                VStack(alignment: .leading) {
+                                    Text(article.title)
+                                        .font(.headline)
+                                        .lineLimit(2)
+                                    Text(article.description)
+                                        .font(.subheadline)
+                                        .lineLimit(10)
+                                }
+                            }
+                        }
                     }
                 }
             }
-        }
-        .onAppear {
-            isLoading = true
-            isThereError = false
-            newsApi.getNews { success in
-                isLoading = false
-                if !success {
-                    isThereError = true
+            .onAppear {
+                isLoading = true
+                isThereError = false
+                newsApi.getNews { success in
+                    isLoading = false
+                    if !success {
+                        isThereError = true
+                    }
                 }
             }
         }
